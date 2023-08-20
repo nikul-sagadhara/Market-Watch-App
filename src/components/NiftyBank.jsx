bc3d447f79a8d03e82e5a0fc1f7357d9f291a9c7
Nikul Sagadhara <nikulsagadhara@gmail.com>
2023-08-20 22:37:25 +0530

    4th commit

--- src/components/NiftyBank.jsx
+++ src/components/NiftyBank.jsx
@@ -6,24 +6,24 @@ const NiftyBank = () => {
   const {marketData} = useNiftyBank();
 
   return !marketData ? <div className='w-full min-h-screen flex justify-center items-start mt-8'><Loader/></div> : (
-    <div className='w-11/12 mx-auto overflow-auto'>
-    <table className="table-auto border-collapse border border-slate-500 my-8 w-full text-xs">
-    <thead className='h-12'>
+    <div className='w-11/12 mx-auto overflow-x-auto'>
+    <table className="border-collapse border border-slate-500 my-8 w-11/12 mx-auto h-full text-xs">
+    <thead className='h-12 sticky top-0 z-10'>
         <tr className='bg-[#3A2D7D] text-white border border-slate-600 text-right'>
-            <th className='text-left pl-4'>SYMBOL</th>
-            <th className=''>LTP</th>
-            <th className=''>CHNG</th>
-            <th className=''>%CHNG</th>
-            <th className=''>OPEN</th>
-            <th className=''>HIGH</th>
-            <th className=''>LOW</th>
-            <th className=''>Y. LOW</th>
-            <th className=''>Y. HIGH</th>
-            <th className=''>PRE. CLOSE</th>
-            <th className=''>VOLUME shares</th>
-            <th className=''>VALUE lakhs</th>
-            <th className=''>%CHNG 365D</th>
-            <th className='pr-4'>%CHNG 30D</th>
+            <th className='bg-[#3A2D7D] text-left pl-4 sticky left-0'>SYMBOL</th>
+            <th className='pr-8'>LTP</th>
+            <th className='pr-8'>CHNG</th>
+            <th className='pr-8'>%CHNG</th>
+            <th className='pr-8'>OPEN</th>
+            <th className='pr-8'>HIGH</th>
+            <th className='pr-8'>LOW</th>
+            <th className='pr-8'>Y. LOW</th>
+            <th className='pr-8'>Y. HIGH</th>
+            <th className='pr-8'>PRE. CLOSE</th>
+            <th className='pr-8'>VOLUME shares</th>
+            <th className='pr-8'>VALUE lakhs</th>
+            <th className='pr-8'>%CHNG 365D</th>
+            <th className='pr-8'>%CHNG 30D</th>
         </tr>
     </thead>
     <tbody className='divide-y divide-slate-600'>
@@ -31,20 +31,20 @@ const NiftyBank = () => {
             marketData?.map(({symbol, lastPrice, change, pChange, open, dayHigh, dayLow, yearHigh, yearLow, previousClose, totalTradedValue, totalTradedVolume, perChange30d, perChange365d, chartTodayPath}, index) => {
                 return (
                     <tr className=' bg-[#E8E8E8] h-8' key={index}>
-                    <td className='text-left pl-4'>{symbol}</td>
-                    <td className='font-bold text-right'>{lastPrice?.toLocaleString("en-IN")}</td>
-                    <td className='text-right'>{change?.toLocaleString("en-IN")}</td>
-                    <td className='text-right'>{pChange?.toLocaleString("en-IN")}</td>
-                    <td className='text-right'>{open?.toLocaleString("en-IN")}</td>
-                    <td className='text-right'>{dayHigh?.toLocaleString("en-IN")}</td>
-                    <td className='text-right'>{dayLow?.toLocaleString("en-IN")}</td>
-                    <td className='text-right'>{yearLow?.toLocaleString("en-IN")}</td>
-                    <td className='text-right'>{yearHigh?.toLocaleString("en-IN")}</td>
-                    <td className='text-right'>{previousClose?.toLocaleString("en-IN")}</td>
-                    <td className='text-right'>{totalTradedVolume?.toLocaleString("en-IN")}</td>
-                    <td className='text-right'>{totalTradedValue?.toLocaleString("en-IN")}</td>
-                    <td className='text-right'>{perChange365d?.toLocaleString("en-IN")}</td>
-                    <td className='text-right pr-4'>{perChange30d?.toLocaleString("en-IN")}</td>
+                    <td className='bg-[#E8E8E8] text-left pl-4 sticky left-0 z-10'>{symbol}</td>
+                    <td className='font-bold text-right pr-8'>{lastPrice?.toLocaleString("en-IN")}</td>
+                    <td className='text-right pr-8'>{change?.toLocaleString("en-IN")}</td>
+                    <td className='text-right pr-8'>{pChange?.toLocaleString("en-IN")}</td>
+                    <td className='text-right pr-8'>{open?.toLocaleString("en-IN")}</td>
+                    <td className='text-right pr-8'>{dayHigh?.toLocaleString("en-IN")}</td>
+                    <td className='text-right pr-8'>{dayLow?.toLocaleString("en-IN")}</td>
+                    <td className='text-right pr-8'>{yearLow?.toLocaleString("en-IN")}</td>
+                    <td className='text-right pr-8'>{yearHigh?.toLocaleString("en-IN")}</td>
+                    <td className='text-right pr-8'>{previousClose?.toLocaleString("en-IN")}</td>
+                    <td className='text-right pr-8'>{totalTradedVolume?.toLocaleString("en-IN")}</td>
+                    <td className='text-right pr-8'>{totalTradedValue?.toLocaleString("en-IN")}</td>
+                    <td className='text-right pr-8'>{perChange365d?.toLocaleString("en-IN")}</td>
+                    <td className='text-right pr-8'>{perChange30d?.toLocaleString("en-IN")}</td>
                 </tr>
                 )
             })
