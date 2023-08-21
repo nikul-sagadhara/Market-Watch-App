33a07c4aa655ee4c840594573f0bad8a983571be
Nikul Sagadhara <nikulsagadhara@gmail.com>
2023-08-21 23:14:44 +0530

    auto update implemented

--- src/utils/useNifty50Data.js
+++ src/utils/useNifty50Data.js
@@ -1,44 +1,52 @@
 import { useState, useEffect } from 'react';
-// import { NIFTY50_API } from './constants';
+import { NIFTY50_API } from './constants';
 
 const useNifty50Data = () => {
     const [marketData, setMarketData] = useState();
 
     useEffect(() => {
-        getMyData();
+        const timer = setInterval(() => {
+            getMyData();
+            console.log("timer");
+        }, 4000)
+
+        return () => {
+            clearInterval(timer);
+            console.log("return");
+        }
     }, [])
 
-    // const getMyData = async () => {
-    //     const url = NIFTY50_API;
-        
-    //     try {
-    //         const response = await fetch(url);
-    //         const result = await response.json();
-    //         setMarketData(result.data);
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // }
-
     const getMyData = async () => {
-        const url = 'https://latest-stock-price.p.rapidapi.com/price?Indices=NIFTY%2050';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'c560269410mshb8694d846f9f12ep16ace2jsn46bafcf6c5c5',
-		'X-RapidAPI-Host': 'latest-stock-price.p.rapidapi.com'
-	}
-};
+        const url = NIFTY50_API;
+        
+        try {
+            const response = await fetch(url);
+            const result = await response.json();
+            setMarketData(result.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-	console.log(result);
-    setMarketData(result);
-} catch (error) {
-	console.error(error);
-}
-    };
+//     const getMyData = async () => {
+//         const url = 'https://latest-stock-price.p.rapidapi.com/price?Indices=NIFTY%2050';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'c560269410mshb8694d846f9f12ep16ace2jsn46bafcf6c5c5',
+// 		'X-RapidAPI-Host': 'latest-stock-price.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.json();
+// 	console.log(result);
+//     setMarketData(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+//     };
 
     return {marketData};
 
--- src/utils/useNiftyFinServ.js
+++ src/utils/useNiftyFinServ.js
@@ -1,48 +1,56 @@
 import { useState, useEffect} from 'react';
-// import { NIFTY_FIN_API } from './constants';
+import { NIFTY_FIN_API } from './constants';
 
 const useNiftyFinServ = () => {
     const [marketData, setMarketData] = useState();
 
     useEffect(() => {
-        getMyData();
+        const timer = setInterval(() => {
+            getMyData();
+            console.log("timer");
+        }, 4000)
+
+        return () => {
+            clearInterval(timer);
+            console.log("return");
+        }
     }, [])
     
-    // const getMyData = async () => {
-    //     const url = NIFTY_FIN_API;
-        
-    //     try {
-    //         const response = await fetch(url);
-    //         const result = await response.json();
-    //         setMarketData(result.data);
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // }
-
-
-
-
-
     const getMyData = async () => {
-        const url = 'https://latest-stock-price.p.rapidapi.com/price?Indices=NIFTY%20FIN%20SERVICE';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'c560269410mshb8694d846f9f12ep16ace2jsn46bafcf6c5c5',
-		'X-RapidAPI-Host': 'latest-stock-price.p.rapidapi.com'
-	}
-};
+        const url = NIFTY_FIN_API;
+        
+        try {
+            const response = await fetch(url);
+            const result = await response.json();
+            setMarketData(result.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-	console.log(result);
-    setMarketData(result);
-} catch (error) {
-	console.error(error);
-}
-    };
+
+
+
+
+//     const getMyData = async () => {
+//         const url = 'https://latest-stock-price.p.rapidapi.com/price?Indices=NIFTY%20FIN%20SERVICE';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'c560269410mshb8694d846f9f12ep16ace2jsn46bafcf6c5c5',
+// 		'X-RapidAPI-Host': 'latest-stock-price.p.rapidapi.com'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.json();
+// 	console.log(result);
+//     setMarketData(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+//     };
 
     return {marketData};
 }
